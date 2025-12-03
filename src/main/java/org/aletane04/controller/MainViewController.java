6d7b5ec26a85b3deaeb21c486fc74c8ef0996a06
AleTane04 <gaetanoalessioforino@gmail.com>
2025-12-03 14:27:12 +0100

    Aggiunti tutti gli scheletri dei Controller e delle View

--- src/main/java/org/aletane04/controller/MainViewController.java
+++ src/main/java/org/aletane04/controller/MainViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.aletane04;
+package org.aletane04.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author 39392
  */
-public class MyLibraryViewController implements Initializable {
+public class MainViewController implements Initializable {
 
     /**
      * Initializes the controller class.
