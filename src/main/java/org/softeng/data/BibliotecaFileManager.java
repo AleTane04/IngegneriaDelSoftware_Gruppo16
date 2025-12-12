834c92eba5a27c645fd165c877ae1939634fa665
francescopiodepiano04-eng <francescopiodepiano04@gmail.com>
2025-12-12 10:05:38 +0100

    Update BibliotecaFileManager.java

--- src/main/java/org/softeng/data/BibliotecaFileManager.java
+++ src/main/java/org/softeng/data/BibliotecaFileManager.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/**
+ * @file BibliotecaFileManager.java
+ * @brief Il file contiene l'implementazione della classe 'BibliotecaFileManager'
+ *
+ * informazioni sul file e il suo ruolo nel progetto
+ */
 package org.softeng.data;
 
 import org.softeng.model.*;
@@ -15,19 +21,37 @@ import java.util.List;
  *
  * @author 39392
  */
+
+/**
+ * @brief Scrive il contenuto di una lista su un file, preceduto da una riga di intestazione.
+ *
+ * Questa funzione scrive l'intestazione specificata, e poi itera sulla lista fornita, 
+ * delegando la formattazione di ciascun oggetto (Libro, Utente o Prestito)
+ * al rispettivo metodo toCSV().
+ *
+ * @pre Il parametro myList deve contenere oggetti che implementano il metodo toCSV() 
+ * o essere vuoto.
+ * @post I dati contenuti in myList e l'intestazione myFileHeader sono stati scritti 
+ * nel file specificato da fileName. In caso di successo, il file viene chiuso.
+ *
+ * @param[in] fileName Il nome del file su cui scrivere i dati.
+ * @param[in] myList La lista generica contenente gli oggetti da salvare 
+ * @param[in] myFileHeader La stringa da utilizzare come intestazione del file.
+ *
+ */
 public class BibliotecaFileManager 
 {
     public void scriviSuFile(String fileName, List<?> myList, String myFileHeader) 
     {
-        /* Uso il try con le risorse */
+        ///< Uso il try con le risorse 
         try(PrintWriter pw = new PrintWriter(new FileWriter(fileName))) 
         {
-            /* Scrivo l'intestazione del mio file */
+            ///< Scrivo l'intestazione del mio file 
             pw.println(myFileHeader);
             
             for(Object obj : myList) 
             {
-                /* Scrivo l'header */
+                ///< Scrivo l'header 
                 if(obj instanceof Libro)
                     pw.println(((Libro) obj).toCSV());
                 else
@@ -43,22 +67,87 @@ public class BibliotecaFileManager
             System.err.println("Errore nella scrittura del file: "+ fileName + "; Errore: "+ e.getMessage());
         }
     }
-    
+
+
+    /**
+     * @brief Salva la lista di tutti gli oggetti Libro su un file specificato.
+     *
+     * Questa funzione è un metodo specifico che delega l'operazione di scrittura 
+     * al metodo generico scriviSuFile(), fornendo l'intestazione standard 
+     * per il file dei libri in formato CSV.
+     *
+     *
+     * @pre La lista libri deve contenere oggetti Libro validi.
+     * @post Il file specificato (fileName) è stato creato o sovrascritto, e contiene 
+     * l'intestazione "TITOLO;AUTORI;DATA;ISBN;COPIE" seguita dai dati di ciascun libro 
+     * in formato CSV.
+     * @param[in] libri La lista di oggetti Libro da rendere persistente.
+     * @param[in] fileName Il nome del file su cui salvare i dati.
+     */
     public void salvaLibri(List<Libro> libri, String fileName) 
     {
         scriviSuFile(fileName, libri,"TITOLO;AUTORI;DATA;ISBN;COPIE");
     }
-    
+
+
+    /**
+     * @brief Salva la lista di tutti gli oggetti Utente su un file specificato.
+     *
+     * Questa funzione è un metodo specifico che delega l'operazione di scrittura 
+     * al metodo generico scriviSuFile(), fornendo l'intestazione standard 
+     * per il file degli utenti in formato CSV.
+     *
+     * @pre La lista utenti deve contenere oggetti Utente validi.
+     * @post Il file specificato (fileName) è stato creato o sovrascritto, e contiene 
+     * l'intestazione "NOME;COGNOME;MATRICOLA;EMAIL" seguita dai dati di ciascun utente 
+     * in formato CSV.
+     * @param[in] utenti La lista di oggetti Utente da rendere persistente.
+     * @param[in] fileName Il nome del file su cui salvare i dati.
+     */
     public void salvaUtenti(List<Utente> utenti, String fileName) 
     {
         scriviSuFile(fileName, utenti,"NOME;COGNOME;MATRICOLA;EMAIL");
     }
-    
+
+
+    /**
+     * @brief Salva la lista di tutti gli oggetti Prestito su un file specificato.
+     *
+     * Questa funzione è un metodo specifico che delega l'operazione di scrittura 
+     * al metodo generico scriviSuFile(), fornendo l'intestazione standard 
+     * per il file dei libri in formato CSV.
+     *
+     *
+     * @pre La lista prestiti deve contenere oggetti Prestito validi.
+     * @post Il file specificato (fileName) è stato creato o sovrascritto, e contiene 
+     * l'intestazione "MATRICOLA_UTENTE;ISBN_LIBRO;DATA_INIZIO;DATA_FINE;DATA_RESTITUZIONE" seguita dai dati di ciascun prestito 
+     * in formato CSV.
+     * @param[in] prestiti La lista di oggetti Prestito da rendere persistente.
+     * @param[in] fileName Il nome del file su cui salvare i dati.
+     */
     public void salvaPrestiti(List<Prestito> prestiti, String fileName) 
     {
         scriviSuFile(fileName, prestiti,"MATRICOLA_UTENTE;ISBN_LIBRO;DATA_INIZIO;DATA_FINE;DATA_RESTITUZIONE");
     }
-    
+
+
+    /**
+     * @brief Carica e deserializza gli oggetti Libro da un file di testo.
+     *
+     * Questa funzione legge il file specificato, saltando la prima riga (intestazione), 
+     * e tenta di creare un nuovo oggetto Libro per ogni riga successiva.
+     * Le righe corrotte (che sollevano un'eccezione durante la costruzione del Libro)
+     * vengono ignorate e segnalate su System.err.
+     *
+     *
+     *
+     * @pre Il file specificato deve esistere. Se non esiste, viene restituita una lista vuota.
+     * @pre Il file, se esistente, deve essere un file di testo con la prima riga come intestazione.
+     *
+     * @return Una lista osservabile contenente tutti gli oggetti Libro 
+     * caricati con successo dal file. La lista è vuota se il file non esiste o è vuoto.
+     *
+     */
     public ObservableList<Libro> caricaLibriDaFile(String fileName) 
     {
         ObservableList<Libro> libri = FXCollections.observableArrayList();
@@ -68,14 +157,14 @@ public class BibliotecaFileManager
 
         try (BufferedReader br = new BufferedReader(new FileReader(f))) 
         {
-            /* Lettura a vuoto, salto l'intestazione */
+            ///< Lettura a vuoto, salto l'intestazione 
             br.readLine(); 
             String riga;
             while ((riga = br.readLine()) != null) 
             {
                 if(riga.isEmpty())
                     continue;
-                /* Aggiungo un nuovo Libro dalla lettura della riga */
+                ///< Aggiungo un nuovo Libro dalla lettura della riga 
                 try
                 {
                     libri.add(new Libro(riga));
@@ -91,7 +180,26 @@ public class BibliotecaFileManager
         }
         return libri;
     }
-    
+
+
+
+    /**
+     * @brief Carica e deserializza gli oggetti Utente da un file di testo.
+     *
+     * Questa funzione legge il file specificato, saltando la prima riga (intestazione), 
+     * e tenta di creare un nuovo oggetto Utente per ogni riga successiva.
+     * Le righe corrotte (che sollevano un'eccezione durante la costruzione del Libro)
+     * vengono ignorate e segnalate su System.err.
+     *
+     *
+     *
+     * @pre Il file specificato deve esistere. Se non esiste, viene restituita una lista vuota.
+     * @pre Il file, se esistente, deve essere un file di testo con la prima riga come intestazione.
+     *
+     * @return Una lista osservabile contenente tutti gli oggetti Utente 
+     * caricati con successo dal file. La lista è vuota se il file non esiste o è vuoto.
+     *
+     */
     public ObservableList<Utente> caricaUtentiDaFile(String nomeFile) 
     {
         ObservableList<Utente> utenti = FXCollections.observableArrayList();
@@ -101,17 +209,17 @@ public class BibliotecaFileManager
 
         try (BufferedReader br = new BufferedReader(new FileReader(f))) 
         {
-            /* Lettura a vuoto, salto l'intestazione */
+            ///< Lettura a vuoto, salto l'intestazione 
             br.readLine();
             String riga;
             while ((riga = br.readLine()) != null) 
             {
                 if(riga.isEmpty())
                     continue;
-                /* Aggiungo un nuovo utente dalla lettura della riga */
+                ///< Aggiungo un nuovo utente dalla lettura della riga 
                 try
                 {
-                    /*  Uso il costruttore CSV */
+                    ///< Uso il costruttore CSV 
                     utenti.add(new Utente(riga));
                 }
                 catch(Exception e)
@@ -126,7 +234,24 @@ public class BibliotecaFileManager
     }
     
 
-    /* Questo metodo ricollega gli ID (ISBN/Matricola) agli oggetti veri. Carica i prestiti e ricostruisce i collegamenti tra gli oggetti*/
+    ///< Questo metodo ricollega gli ID (ISBN/Matricola) agli oggetti veri. Carica i prestiti e ricostruisce i collegamenti tra gli oggetti
+    /**
+     * @brief Carica e deserializza gli oggetti Prestito da un file di testo.
+     *
+     * Questa funzione legge il file specificato, saltando la prima riga (intestazione), 
+     * e tenta di creare un nuovo oggetto Prestito per ogni riga successiva.
+     * Le righe corrotte (che sollevano un'eccezione durante la costruzione del Libro)
+     * vengono ignorate e segnalate su System.err.
+     *
+     *
+     *
+     * @pre Il file specificato deve esistere. Se non esiste, viene restituita una lista vuota.
+     * @pre Il file, se esistente, deve essere un file di testo con la prima riga come intestazione.
+     *
+     * @return Una lista osservabile contenente tutti gli oggetti Prestito 
+     * caricati con successo dal file. La lista è vuota se il file non esiste o è vuoto.
+     *
+     */
     public ObservableList<Prestito> caricaPrestitiDaFile(String nomeFile, ObservableList<Utente> utenti, ObservableList<Libro> libri) 
     {
         ObservableList<Prestito> listaPrestiti = FXCollections.observableArrayList();
@@ -136,15 +261,17 @@ public class BibliotecaFileManager
 
         try (BufferedReader br = new BufferedReader(new FileReader(f))) 
         {
-            /* Lettura a vuoto, salto l'intestazione */
+            ///< Lettura a vuoto, salto l'intestazione 
             br.readLine(); 
             String riga;
             while ((riga = br.readLine()) != null) 
             {
                 if (riga.isEmpty()) 
                     continue;
-        /* Ogni volta che viene trovato un ";" 
-        spezzo la stringa e inserisco il contenuto in un array di Stringhe */
+        /** 
+         * Ogni volta che viene trovato un ";" 
+         * spezzo la stringa e inserisco il contenuto in un array di Stringhe 
+         */
                 String[] chunks = riga.split(";");
                 if (chunks.length < 4) continue;
 
@@ -153,35 +280,35 @@ public class BibliotecaFileManager
                 LocalDate dataInizio = LocalDate.parse(chunks[2]);
                 LocalDate dataFine = chunks.length > 3 ? LocalDate.parse(chunks[3]) : LocalDate.now();
 
-                // ---------------------------------------------------------
-                // 1. RICERCA DELL'UTENTE (Sostituzione Stream -> Ciclo For)
-                // ---------------------------------------------------------
+                ///< ---------------------------------------------------------
+                ///< 1. RICERCA DELL'UTENTE (Sostituzione Stream -> Ciclo For)
+                ///< ---------------------------------------------------------
                 Utente utenteTrovato = null;
 
-                // Scorro la lista degli utenti caricata in memoria
+                ///< Scorro la lista degli utenti caricata in memoria
                 for (Utente u : utenti) {
-                    // Se la matricola dell'utente corrente corrisponde a quella scritta nel file prestiti...
+                    ///< Se la matricola dell'utente corrente corrisponde a quella scritta nel file prestiti...
                     if (u.getMatricola().equals(matricolaCercata)) {
-                        utenteTrovato = u; // ...ho trovato l'oggetto giusto!
-                        break;             // Interrompo il ciclo, inutile continuare
+                        utenteTrovato = u; ///< ...ho trovato l'oggetto giusto!
+                        break;             ///< Interrompo il ciclo, inutile continuare
                     }
                 }
 
-                // ---------------------------------------------------------
-                // 2. RICERCA DEL LIBRO (Sostituzione Stream -> Ciclo For)
-                // ---------------------------------------------------------
+                ///< ---------------------------------------------------------
+                ///< 2. RICERCA DEL LIBRO (Sostituzione Stream -> Ciclo For)
+                ///< ---------------------------------------------------------
                 Libro libroTrovato = null;
 
-                // Scorro la lista dei libri caricata in memoria
+                ///< Scorro la lista dei libri caricata in memoria
                 for (Libro l : libri) {
-                    // Se l'ISBN del libro corrente corrisponde a quello scritto nel file prestiti...
+                    ///< Se l'ISBN del libro corrente corrisponde a quello scritto nel file prestiti...
                     if (l.getCodiceISBN().equals(isbnCercato)) {
-                        libroTrovato = l; // ...ho trovato l'oggetto giusto!
-                        break;            // Interrompo il ciclo
+                        libroTrovato = l; ///< ...ho trovato l'oggetto giusto!
+                        break;            ///< Interrompo il ciclo
                     }
                 }
 
-                // 3. Se esistono entrambi, ricreo il prestito
+                ///< 3. Se esistono entrambi, ricreo il prestito
                 try {
                         if (utenteTrovato != null && libroTrovato != null)
                         {
@@ -190,7 +317,7 @@ public class BibliotecaFileManager
                             if(chunks.length >= 5)
                             {
                                 String dataRestString = chunks[4];
-                                /* Se la data è diversa da "null", allora è valida */
+                                ///< Se la data è diversa da "null", allora è valida 
                                 if (!dataRestString.equals("null") && !dataRestString.isEmpty())
                                 {
                                     myPrestito.setDataRestituzioneEffettiva(LocalDate.parse(dataRestString));
