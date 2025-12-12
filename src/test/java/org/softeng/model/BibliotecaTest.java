f8973391262f02b23022f2a672b3c49748b13a63
francescopiodepiano04-eng <francescopiodepiano04@gmail.com>
2025-12-12 15:49:40 +0100

    Create BibliotecaTest.java

---
+++ src/test/java/org/softeng/model/BibliotecaTest.java
@@ -0,0 +1,21 @@
+package org.softeng.model;
+
+import java.time.LocalDate;
+import static java.time.LocalDate.of;
+import java.time.Month;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BibliotecaTest{
+  private Biblioteca biblioteca;
+
+  @Test 
+  publix void testCostruttore(){
+    Biblioteca b = new Biblioteca()
+  }
+}
