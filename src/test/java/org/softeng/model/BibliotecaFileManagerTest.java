3b70950af93ff7b9b6ce4e68b8a314006e53577b
francescopiodepiano04-eng <francescopiodepiano04@gmail.com>
2025-12-13 17:42:44 +0100

    Create BibliotecaFileManagerTest.java

---
+++ src/test/java/org/softeng/model/BibliotecaFileManagerTest.java
@@ -0,0 +1,22 @@
+package org.softeng.data;
+
+import org.softeng.model.Libro;
+import org.softeng.model.Prestito;
+import org.softeng.model.Utente;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BibliotecaFileManagerTest{
+  private BibliotecaFileManager fileManager;
+
+  
+}
